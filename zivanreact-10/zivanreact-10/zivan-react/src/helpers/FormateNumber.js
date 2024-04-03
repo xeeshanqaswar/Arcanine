331f7e399e36eb49fe759eec40f1d9ebca9b5b24
Zeeshan Qaswar <xeeshanqaswar@gmail.com>
2024-04-03 19:59:25 +0500

    Arcanine Website Update.

--- zivanreact-10/zivanreact-10/zivan-react/src/helpers/FormateNumber.js
+++
@@ -1,8 +0,0 @@
-const FormateNumber = ({ number }) => {
-  return Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    maximumFractionDigits: 2,
-  }).format(number);
-};
-export default FormateNumber;
